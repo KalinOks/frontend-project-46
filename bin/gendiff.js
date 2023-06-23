284913a647acf7c6f0b88c5631a626b68f9c7d11
Kalinina Oksana <oksana108@gmail.com>
2023-06-23 17:40:30 +0400

    edit usage information

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,10 @@
 import { program } from 'commander';
 
 program.version('1.0.0');
-
 program.addHelpText('before', 'Compares two configuration files and shows a difference.');
+
+program.argument('<filepath1>');
+program.argument('<filepath2>');
+program.option('-f, --format <type>', 'output format');
 
 program.parse();
