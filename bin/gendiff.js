ea932554edd1fade03a93ff657dd569cc28e9156
Kalinina Oksana <oksana108@gmail.com>
2023-06-25 17:28:10 +0400

    add create diff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import path from 'path';
+import createDiff from '../index.js';
 
 program.version('1.0.0');
 program.addHelpText('before', 'Compares two configuration files and shows a difference.');
@@ -8,5 +10,13 @@ program.addHelpText('before', 'Compares two configuration files and shows a diff
 program.argument('<filepath1>');
 program.argument('<filepath2>');
 program.option('-f, --format <type>', 'output format');
+
+program.action((filepath1, filepath2) => {
+  const absPath1 = path.resolve(process.cwd(), filepath1);
+  const absPath2 = path.resolve(process.cwd(), filepath2);
+  const diff = createDiff(absPath1, absPath2);
+
+  console.log(diff);
+});
 
 program.parse();
---
+++ index.js
@@ -0,0 +1,31 @@
+import { readFileSync } from 'node:fs';
+import _ from 'lodash';
+
+export default (filePath1, filePath2) => {
+  const obj1 = JSON.parse(readFileSync(filePath1));
+  const obj2 = JSON.parse(readFileSync(filePath2));
+
+  const keys1 = _.orderBy(Object.keys(obj1), (key) => key, ['asc']);
+  const keys2 = _.orderBy(Object.keys(obj2), (key) => key, ['asc']);
+
+  const firstFileChanges = keys1.reduce((acc, key) => {
+    if (!keys2.includes(key)) {
+      acc[`- ${key}`] = obj1[key];
+    } else if (obj1[key] === obj2[key]) {
+      acc[`  ${key}`] = obj1[key];
+    } else {
+      acc[`- ${key}`] = obj1[key];
+      acc[`+ ${key}`] = obj2[key];
+    }
+    return acc;
+  }, {});
+
+  const secondFileChanges = _.filter(keys2, (key) => !keys1.includes(key)).reduce((acc, key) => {
+    acc[`+ ${key}`] = obj2[key];
+    return acc;
+  }, {});
+
+  const allChanges = { ...firstFileChanges, ...secondFileChanges };
+
+  return `{\n${Object.entries(allChanges).map(([key, value]) => `    ${key}: ${value}`).join('\n')}\n}`;
+};
