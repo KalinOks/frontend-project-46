884942874fd7a3b66c26f56a011067cfa0156b51
Kalinina Oksana <oksana108@gmail.com>
2023-06-27 18:00:01 +0400

    check eslint

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ export default (filePath1, filePath2) => {
 
   const keys1 = _.orderBy(Object.keys(obj1), (key) => key, ['asc']);
   const keys2 = _.orderBy(Object.keys(obj2), (key) => key, ['asc']);
-
+  var test = 23;
   const firstFileChanges = keys1.reduce((acc, key) => {
     if (!keys2.includes(key)) {
       acc[`- ${key}`] = obj1[key];
